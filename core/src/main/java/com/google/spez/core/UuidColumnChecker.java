f41fff9f7becacd73e46503effc67ef7d326ee09
mhadi4194 <93664484+mhadi4194@users.noreply.github.com>
2022-05-10 09:27:46 -0400

    uuid column as INT64

--- core/src/main/java/com/google/spez/core/UuidColumnChecker.java
+++ core/src/main/java/com/google/spez/core/UuidColumnChecker.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,57 +14,71 @@
  * limitations under the License.
  */
 
+
+
 package com.google.spez.core;
+
+
 
 import com.google.spez.spanner.RowCursor;
 
+
+
 @SuppressWarnings("PMD.BeanMembersShouldSerialize")
 class UuidColumnChecker {
-  private final SpezConfig.SinkConfig config;
-  private boolean tableExists = false;
-  private boolean columnExists = false;
-  private boolean columnIsPrimaryKey = false;
-  private boolean columnIsStringMax = false;
+    private final SpezConfig.SinkConfig config;
+    private boolean tableExists = false;
+    private boolean columnExists = false;
+    private boolean columnIsPrimaryKey = false;
+    private boolean columnIsINT64 = false;
 
-  public UuidColumnChecker(SpezConfig.SinkConfig config) {
-    this.config = config;
-  }
 
-  public void checkRow(RowCursor rc) {
-    tableExists = true;
-    if (rc.getString("COLUMN_NAME").equals(config.getUuidColumn())) {
-      columnExists = true;
-      if (rc.getString("INDEX_TYPE").equals("PRIMARY_KEY")) {
-        columnIsPrimaryKey = true;
-      }
-      if (rc.getString("SPANNER_TYPE").equals("STRING(MAX)")) {
-        columnIsStringMax = true;
-      }
-    }
-  }
 
-  public void throwIfInvalid() {
-    if (tableExists && columnExists && columnIsPrimaryKey && columnIsStringMax) {
-      return;
+    public UuidColumnChecker(SpezConfig.SinkConfig config) {
+        this.config = config;
     }
 
-    String tableDescription = (tableExists ? "it does" : "it doesn't");
-    String columnDescription = (columnExists ? "it does" : "it doesn't");
-    String pkDescription = (columnIsPrimaryKey ? "it is" : "it is not");
-    String typeDescription = (columnIsStringMax ? "it is" : "it is not");
-    throw new IllegalStateException(
-        "Spanner table '"
-            + config.tablePath()
-            + "' must exist ("
-            + tableDescription
-            + ") and contain a column named '"
-            + config.getUuidColumn()
-            + "' ("
-            + columnDescription
-            + ") which is a PRIMARY_KEY ("
-            + pkDescription
-            + ") and is of type STRING(MAX) ("
-            + typeDescription
-            + ")");
-  }
+
+
+    public void checkRow(RowCursor rc) {
+        tableExists = true;
+        if (rc.getString("COLUMN_NAME").equals(config.getUuidColumn())) {
+            columnExists = true;
+            if (rc.getString("INDEX_TYPE").equals("PRIMARY_KEY")) {
+                columnIsPrimaryKey = true;
+            }
+            if (rc.getString("SPANNER_TYPE").equals("INT64")) {
+                columnIsINT64 = true;
+            }
+        }
+    }
+
+
+
+    public void throwIfInvalid() {
+        if (tableExists && columnExists && columnIsPrimaryKey && columnIsINT64) {
+            return;
+        }
+
+
+
+        String tableDescription = (tableExists ? "it does" : "it doesn't");
+        String columnDescription = (columnExists ? "it does" : "it doesn't");
+        String pkDescription = (columnIsPrimaryKey ? "it is" : "it is not");
+        String typeDescription = (columnIsINT64 ? "it is" : "it is not");
+        throw new IllegalStateException(
+                "Spanner table '"
+                        + config.tablePath()
+                        + "' must exist ("
+                        + tableDescription
+                        + ") and contain a column named '"
+                        + config.getUuidColumn()
+                        + "' ("
+                        + columnDescription
+                        + ") which is a PRIMARY_KEY ("
+                        + pkDescription
+                        + ") and is of type INT64 ("
+                        + typeDescription
+                        + ")");
+    }
 }
